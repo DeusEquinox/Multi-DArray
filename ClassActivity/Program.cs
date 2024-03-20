da66eefc7a6542772f86048d3eed8b0bc7512d30
DeiEquinox <144390923+DeusEquinox@users.noreply.github.com>
2024-03-20 12:43:46 +0200

    Completed the program yesterday

--- ClassActivity/Program.cs
+++ ClassActivity/Program.cs
@@ -10,7 +10,22 @@ namespace ClassActivity
     {
         static void Main(string[] args)
         {
+            var members = new[]
+            {
+                new{Number = 1, name = "Mpho Machailo", gender = "Male", DOB = "10/08/2004"},
+                new{Number = 2, name = "Shaz Nkosi", gender = "Female", DOB = "30/08/2002"},
+                new{Number = 3, name = "Yandisa Mdoda", gender = "Male", DOB = "18/07/2003"}
+            };
 
+            Console.WriteLine("Student Information:");
+
+            for (int i = 0; i < members.Length; i++)
+            {
+                Console.WriteLine($"Student {members[i].Number}:\n" +
+                    $"Name: {members[i].name}\n" +
+                    $"Gender: {members[i].gender}\n" +
+                    $"DOB {members[i].DOB}\n");
+            }
         }
     }
 }
